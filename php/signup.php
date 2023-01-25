0541f41b9186ca0e9a706a6fcc843307c4553b4e
HugoEskes <97437930+HugoEskes@users.noreply.github.com>
2023-01-25 15:47:03 +0100

    Connectie tussen website en database test

---
+++ php/signup.php
@@ -0,0 +1,12 @@
+<?php
+include "connection.php";
+
+$firstname = mysqli_real_escape_string($connection, htmlspecialchars($POST['firstname']));
+$lastname = mysqli_real_escape_string($connection, htmlspecialchars($POST['lastname']));
+$email = mysqli_real_escape_string($connection, htmlspecialchars($_POST['email']));
+$username = mysqli_real_escape_string($connection, htmlspecialchars($_POST['username']));
+$password = mysqli_real_escape_string($connection, htmlspecialchars($_POST['password']));
+
+$sql = "INSERT INTO gebruikers (voornaam, achternaam, email, gebruikersnaam, wachtwoord) VALUES ("$firstname", "$lastname", "$email", "$username", "$password")";
+mysqli_query($connection, $sql);
+?>
