1965c24f94382f8c2fb7b40d1505debc22b36c7e
HugoEskes <97437930+HugoEskes@users.noreply.github.com>
2023-01-26 11:44:18 +0100

    sign up pagina's bij elkaar gevoegd voor gemak

--- Pagina's/signup.php
+++ Pagina's/signup.php
@@ -1,3 +1,62 @@
+<?php
+
+$exists = false;
+$showError = false;
+$showAlert = false;
+
+include "connection.php";
+
+// Alle informatie ophalen
+$firstname = mysqli_real_escape_string($connection, htmlspecialchars($_POST['firstname']));
+$lastname = mysqli_real_escape_string($connection, htmlspecialchars($_POST['lastname']));
+$email = mysqli_real_escape_string($connection, htmlspecialchars($_POST['email']));
+$username = mysqli_real_escape_string($connection, htmlspecialchars($_POST['username']));
+$password = mysqli_real_escape_string($connection, htmlspecialchars($_POST['password']));
+$cpassword = mysqli_real_escape_string($connection, htmlspecialchars($_POST['cpassword']));
+
+// Checken of de gebruikernaam al bestaat
+$sql = "Select * from gebruikers where gebruikersnaam='$username'";
+$result = mysqli_query($connection, $sql);
+$num = mysqli_num_rows($result);
+
+if($num!=0)
+{
+    echo "Username not available";
+}
+
+
+// Checken of het email al in gebruik is
+$sql = "Select * from gebruikers where email='$email'";
+$result = mysqli_query($connection, $sql);
+$num = mysqli_num_rows($result);
+
+if($num!=0)
+{
+    echo "This emailaddress is already in use";
+}
+
+
+// Als de wachtwoorden overeen komen wordt het wachtwoord versleuteld en naar de database gestuurd
+//
+if(($password == $cpassword) && $exists==false)
+    {
+    // wachtwoord versleutelen
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+    //informatie in de database zetten
+    $sql = "INSERT INTO gebruikers (voornaam, achternaam, email, gebruikersnaam, wachtwoord) VALUES ('$firstname','$lastname', '$email', '$username', '$hashed_password')";
+    mysqli_query($connection, $sql);
+
+    // gebruiker naar homepagina sturen
+    header('location: /../index.html');
+    }
+// Als de wachtwoorden niet overeen komen wordt dit aangegeven
+else 
+    {
+    echo "Passwords don't match";
+    }
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -61,7 +120,7 @@
       <div class="col-md-12">
         <h2>Register</h2>
         <p>please fill in this form to create an account.</p>
-        <form action="/php/signup.php" method="post">
+        <form action="signup.php" method="post">
           <div class="form-group"> 
             <label>First Name</label>
             <input type="text" id="firstname" name="firstname" class="form-control" required>
