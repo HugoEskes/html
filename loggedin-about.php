f7599c080e7fddb7b78fef80da523533bb592251
Arthurvc-uva <Arthur.van.campenhout@student.uva.nl>
2023-01-30 19:07:57 +0100

    ff checken

--- loggedin-about.php
+++ loggedin-about.php
@@ -1,10 +1,11 @@
-<?php
-session_start();
 
-if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
+<?php
+// session_start();
+
+// if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
   // User is not logged in, redirect to login page with a flag indicating the user was not logged in
-  header('Location: login.php?not_logged_in=1');
-}
+//  header('Location: login.php?not_logged_in=1');
+// }
 ?>
 
 <!DOCTYPE html>
