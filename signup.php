63ec97c05b989bbdc96c31d57fd3d237fbb37c06
Arthurvc-uva <Arthur.van.campenhout@student.uva.nl>
2023-01-30 13:00:26 +0100

    nog meer breaklines

--- signup.php
+++ signup.php
@@ -121,33 +121,39 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     <link href="css/style.min.css" rel="stylesheet">
 </head>
 <body>
-    <!-- Navbar Start -->
-    <div class="container-fluid p-0 nav-bar">
-        <nav class="navbar navbar-expand-lg bg-none navbar-dark py-3">
-            <a href="index.html" class="navbar-brand px-lg-4 m-0">
-                <h1 class="m-0 display-4 text-uppercase text-white">Ski. I. P</h1>
-            </a>
-            <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
-                <div class="navbar-nav ml-auto p-4">
-                    <a href="index.html" class="nav-item nav-link">Home</a>
-                    <a href="reservation.html" class="nav-item nav-link">Reserve</a>
-                    <a href="about.html" class="nav-item nav-link">About us</a>
-                    <div class="nav-item dropdown">
-                        <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">My Ski. I. P</a>
-                        <div class="dropdown-menu text-capitalize">
-                            <a href="login.html" class="dropdown-item">Login</a>
-                            <a href="signup.php" class="dropdown-item active">Signup</a>
-                        </div>
+
+<!-- Navbar Start -->
+<div class="container-fluid p-0 nav-bar">
+    <nav class="navbar navbar-expand-lg bg-none navbar-dark py-3">
+        <a href="index.html" class="navbar-brand px-lg-4 m-0">
+            <h1 class="m-0 display-4 text-uppercase text-white">Ski. I. P</h1>
+        </a>
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+            <div class="navbar-nav ml-auto p-4">
+                <a href="index.html" class="nav-item nav-link">Home</a>
+                <a href="reservation.html" class="nav-item nav-link">Reserve</a>
+                <a href="about.html" class="nav-item nav-link">About us</a>
+                <div class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">My Ski. I. P</a>
+                    <div class="dropdown-menu text-capitalize">
+                        <a href="login.html" class="dropdown-item">Login</a>
+                        <a href="signup.php" class="dropdown-item active">Signup</a>
                     </div>
-                    
                 </div>
+                
             </div>
-        </nav>
-    </div>
-    <!-- Navbar End -->
+        </div>
+    </nav>
+</div>
+<!-- Navbar End -->
+
+<br>
+<br>
+<br>
+<br>
 <br>
   <div class="container">
     <div class="row">
@@ -166,7 +172,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
           </div>
           <br>
           <div class="form-group">
-            <label>E-mail Adress</label>
+            <label>E-mail Address</label>
             <input type="text" id="Email" name="email" class="form-control" required>
           </div>
           <br>
