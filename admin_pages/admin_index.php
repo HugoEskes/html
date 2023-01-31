43733614ff8e9eaa5682adba05ee234ced51c5b3
Arthurvc-uva <Arthur.van.campenhout@student.uva.nl>
2023-01-31 11:17:52 +0100

    Beetje aangepast

--- admin_pages/admin_index.php
+++ admin_pages/admin_index.php
@@ -2,7 +2,7 @@
   session_start();
   if (!isset($_SESSION['admin']) || $_SESSION['admin'] !== true) {
     // User is not logged in, redirect to login page with a flag indicating the user was not logged in
-    header('Location: admin_login.php?not_logged_in=1');
+    header('Location: ../admin_login.php?not_logged_in=1');
   }
 
   echo "<script>alert('Welcome, " . $_SESSION['naam'] . "!');</script>";
