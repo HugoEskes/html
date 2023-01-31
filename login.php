919e1aad28401a8049454ad89a4376e470aeec5b
Arthurvc-uva <Arthur.van.campenhout@student.uva.nl>
2023-01-31 17:59:43 +0100

    navbar gefixt

--- login.php
+++ login.php
@@ -66,33 +66,27 @@ mysqli_close($connection);
     <link href="css/style.min.css" rel="stylesheet">
     </head>
     <body>
-        <!-- Navbar Start -->
-        <div class="container-fluid p-0 nav-bar">
-        <nav class="navbar navbar-expand-lg bg-none navbar-dark py-3">
-            <a href="index.html" class="navbar-brand px-lg-4 m-0">
-                <h1 class="m-0 display-4 text-uppercase text-white">Ski. I. P</h1>
-            </a>
-            <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
-                <div class="navbar-nav ml-auto p-4">
-                    <a href="index.html" class="nav-item nav-link">Home</a>
-                    <a href="reservation.html" class="nav-item nav-link">Reserve</a>
-                    <a href="about.html" class="nav-item nav-link">About us</a>
-                    <div class="nav-item dropdown">
-                        <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">My Ski. I. P</a>
-                        <div class="dropdown-menu text-capitalize">
-                            <a href="login.php" class="dropdown-item active">Login</a>
-                            <a href="signup.php" class="dropdown-item">Signup</a>
-                        </div>
-                    </div>
-                    
-                </div>
+<!-- Navbar Start -->
+<div class="container-fluid p-0 nav-bar">
+    <nav class="navbar navbar-expand-lg bg-none navbar-dark py-3">
+        <a href="index.html" class="navbar-brand px-lg-4 m-0">
+            <h1 class="m-0 display-4 text-uppercase text-white">Ski. I. P.</h1>
+        </a>
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+            <div class="navbar-nav ml-auto p-4">
+                <a href="index.html" class="nav-item nav-link">Home</a>
+                <a href="reservation.html" class="nav-item nav-link">Reserve</a>
+                <a href="about.html" class="nav-item nav-link">About us</a>
+                <a href="login.php" class="nav-item nav-link active">Log In</a>
+                <a href="signup.php" class="nav-item nav-link">Sign Up</a>
             </div>
-        </nav>
-    </div>
-    <!-- Navbar End -->
+        </div>
+    </nav>
+</div>
+<!-- Navbar End -->
 
     <!-- Page Header Start -->
 <div class="container-fluid page-header mb-5 position-relative overlay-bottom">
