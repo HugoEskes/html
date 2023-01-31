0a84f0d38fcd5702ea652ca706c1c8181da86812
Arthurvc-uva <Arthur.van.campenhout@student.uva.nl>
2023-01-31 15:48:17 +0100

    admin pagina's aangepast

---
+++ admin_pages/edit.php
@@ -0,0 +1,10 @@
+<?php
+  require_once '../php/connection.php'
+  // Check if connection was successful
+  if (!$connection) {
+    die("Connection failed: " . mysqli_connect_error());
+  }
+  
+  // Update query
+  $update_query = "UPDATE gebruikers SET name='" . $_POST['name'] . "', email='"
+  ?>
