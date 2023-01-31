e62e5c1aa9bfa8cd82ab875e1abf1eba47132c7a
Arthurvc-uva <Arthur.van.campenhout@student.uva.nl>
2023-01-31 17:28:34 +0100

    admin

--- admin_pages/admin_index.php
+++ admin_pages/admin_index.php
@@ -50,9 +50,8 @@
                     <a href="admin_index.php" class="nav-item nav-link active">Home</a>
                     <a href="admin_reservations.php" class="nav-item nav-link">Reservations</a>
                     <a href="admin_users.php" class="nav-item nav-link">Other users</a>
-                    <a href="admin_user.php" class="nav-item nav-link">Account</a>
+                    <a href="admin_account.php" class="nav-item nav-link">Account</a>
                     <a href="../php/logout.php" class="nav-item nav-link">Logout</a>
-                    
                 </div>
             </div>
         </nav>
