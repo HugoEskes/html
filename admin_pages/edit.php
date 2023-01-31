22c7268724d3bdf0d5acca50caaaba6656928b38
Arthurvc-uva <Arthur.van.campenhout@student.uva.nl>
2023-01-31 15:55:07 +0100

    admin dingen geprobeerd

--- admin_pages/edit.php
+++ admin_pages/edit.php
@@ -5,6 +5,27 @@
     die("Connection failed: " . mysqli_connect_error());
   }
   
-  // Update query
-  $update_query = "UPDATE gebruikers SET name='" . $_POST['name'] . "', email='"
+  if (isset($_POST['edit'])) {
+    // Get the form data
+    $id = $_POST['gebruikerID'];
+    $name = $_POST['gebruikersnaam'];
+    $email = $_POST['email'];
+    
+    // Update query
+    $update_query = "UPDATE gebruikers SET name='$name', email='$email' WHERE id='$id'";
+    $result = mysqli_query($connection, $update_query);
+    
+    // Check if update was successful
+    if ($result) {
+      // Redirect the user back to the main page
+      header('Location: admin_users.php');
+      exit;
+    } else {
+      echo "Error updating record: " . mysqli_error($connection);
+    }
+  }
+  
+  // Close the database connection
+  mysqli_close($connection);
+  
   ?>
