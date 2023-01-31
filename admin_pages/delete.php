0a570e415c98fcdae3f94fa8fec0c0c0428c43a4
Arthurvc-uva <Arthur.van.campenhout@student.uva.nl>
2023-01-31 16:06:02 +0100

    admin

--- admin_pages/delete.php
+++ admin_pages/delete.php
@@ -1,18 +1,18 @@
 <?php
   require_once '../php/connection.php';
-  // Check if connection was successful
+  // Check connection
   if (!$connection) {
     die("Connection failed: " . mysqli_connect_error());
   }
-  
+
   if (isset($_POST['delete'])) {
     // Get the form data
-    $id = $_POST['gebruikerID'];
-    
+    $id = $_POST['id'];
+
     // Delete query
     $delete_query = "DELETE FROM gebruikers WHERE gebruikerID='$id'";
     $result = mysqli_query($connection, $delete_query);
-    
+
     // Check if delete was successful
     if ($result) {
       // Redirect the user back to the main page
@@ -22,7 +22,7 @@
       echo "Error deleting record: " . mysqli_error($connection);
     }
   }
-  
+
   // Close the database connection
   mysqli_close($connection);
 ?>
