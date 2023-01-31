06db2ea64155f6a7d3997c61e0b3e4c907efe0e9
Arthurvc-uva <Arthur.van.campenhout@student.uva.nl>
2023-01-31 15:50:32 +0100

    foutje gefixt 3.0

--- admin_pages/edit.php
+++ admin_pages/edit.php
@@ -1,5 +1,5 @@
 <?php
-  require_once '../php/connection.php'
+  require_once '../php/connection.php';
   // Check if connection was successful
   if (!$connection) {
     die("Connection failed: " . mysqli_connect_error());
