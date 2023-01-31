35bf05c62c4cee8f3cd76a854df4ca50f73e8840
Arthurvc-uva <Arthur.van.campenhout@student.uva.nl>
2023-01-31 18:10:07 +0100

    index

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <?php
   session_start();
   if (isset($_SESSION['logged_in']) || $_SESSION['logged_in'] == true) {
-    // User is  logged in, redirect to loggedin index page with a flag indicating the user was not logged in
+    // User is  logged in, redirect to loggedin index page
     header('Location: loggedin-index.php');
+  }
+if (isset($_SESSION['admin']) || $_SESSION['admin'] == true) {
+    // User is  logged in, redirect to admin index page
+    header('Location: admin_pages/admin-index.php');
   }
 ?>
 <!DOCTYPE html>
