91a754a571d45096d101014406dec73ece318acd
Arthurvc-uva <Arthur.van.campenhout@student.uva.nl>
2023-01-31 17:30:53 +0100

    admin

---
+++ admin_pages/welcome.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+  if (!isset($_SESSION['admin']) || $_SESSION['admin'] !== true) {
+    // User is not logged in, redirect to login page with a flag indicating the user was not logged in
+    header('Location: ../admin_login.php?not_logged_in=1');
+  }
+else {
+  echo "<script>alert('Welcome, " . $_SESSION['naam'] . "!');</script>";
+  header('Location: admin_index.php')
+}
+  ?>
