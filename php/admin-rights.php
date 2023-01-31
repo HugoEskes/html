60e4d41d9316eb799870a5e138338991a6a8d852
Arthurvc-uva <Arthur.van.campenhout@student.uva.nl>
2023-01-31 12:49:21 +0100

    admin-rights aangepast

--- php/admin-rights.php
+++ php/admin-rights.php
@@ -8,13 +8,13 @@ if ($connection->connect_error) {
 }
 
 // Select all users from the 'admins' table
-$sql = "SELECT gebruikersnaam FROM admins";
+$sql = "SELECT email FROM admins";
 $result = $connection->query($sql);
 
 if ($result->num_rows > 0) {
     // Loop through all rows in the 'admins' table
     while ($row = $result->fetch_assoc()) {
-        $username = $row["gebruikersnaam"];
+        $username = $row["email"];
         $sql = "GRANT SELECT, INSERT, UPDATE, DELETE ON gebruikers.* TO '$username'@'localhost'";
         if ($connection->query($sql) === TRUE) {
             echo "Privileges granted to user $username\n";
