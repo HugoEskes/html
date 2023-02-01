6b93c0a4072066237d9241e2d590e6c73c7209e5
Arthurvc-uva <Arthur.van.campenhout@student.uva.nl>
2023-02-01 14:49:31 +0100

    edit.php

--- edit.php
+++ edit.php
@@ -38,7 +38,7 @@ if (isset($_POST['submit'])) {
   $update_query = "UPDATE gebruikers SET voornaam = '$updated_voornaam', achternaam = '$updated_achternaam', gebruikersnaam = '$updated_gebruikersnaam', email = '$updated_email', wachtwoord = '$updated_wachtwoord' WHERE gebruikerID = '$id'";
 
   if (mysqli_query($connection, $update_query)) {
-    echo "<script>alert('" . $_SESSION['naam'] . ", your data has been succesfully changed!');</script>";
+    echo "<script>alert('" . $updated_voornaam . ", your data has been succesfully changed!');</script>";
     header("refresh:0.5; url = loggedin-account.php");
   } else {
     echo "Error updating data: " . mysqli_error($connection);
