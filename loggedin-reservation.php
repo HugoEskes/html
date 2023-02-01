76fda3c9d24a768a41bb8830408e2b56da2d1572
Rafael Beekman <rafael.beekman@gmail.com>
2023-02-01 17:14:30 +0100

    reserve

--- loggedin-reservation.php
+++ loggedin-reservation.php
@@ -99,11 +99,6 @@ if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
                                     </div>
                                 </div>
                                 <div class="form-group">
-                                    <div class="time" id="time" data-target-input="nearest">
-                                        <input type="text" class="form-control bg-transparent border-primary p-4 datetimepicker-input" placeholder="Time" data-target="#time" data-toggle="datetimepicker"/>
-                                    </div>
-                                </div>
-                                <div class="form-group">
                                     <select name="time_slot" class="custom-select bg-transparent border-primary px-4" style="height: 49px;">
                                         <option value="">Select a time slot</option>
                                         <?php
