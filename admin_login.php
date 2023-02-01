37251f00dbd93a2c72f60d263a27d9cf722b6407
HugoEskes <97437930+HugoEskes@users.noreply.github.com>
2023-02-01 18:10:16 +0100

    Bericht test

--- admin_login.php
+++ admin_login.php
@@ -2,10 +2,9 @@
 require_once 'php/connection.php';
 require_once "php/session.php";
 
-if ($_GET['status'] == 'incorrect')
-   echo "";
-
-
+if ($_GET['status'] == 'incorrect') {
+    echo("<script type='text/JavaScript'> alert('Emailaddress or password incorrect'); </script>");
+}
 
 if (isset($_POST['email']) && isset($_POST['password'])) {
     $email = mysqli_real_escape_string($connection, htmlspecialchars($_POST['email']));
@@ -16,8 +15,9 @@ if (isset($_POST['email']) && isset($_POST['password'])) {
     $result = mysqli_query($connection, $sql);
     $row = mysqli_fetch_assoc($result);
     
-    if (empty($result)) {
+    if (mysqli_num_rows($result) == 0) {
         header("Location: admin_login.php?status=incorrect");
+        exit;
     }
     // Check if email and password match
     if ($row['email'] == $email && $row['wachtwoord'] == $password) {
@@ -37,6 +37,7 @@ if (isset($_POST['email']) && isset($_POST['password'])) {
     // Login failed
     // Display an error message
     header("Location: admin_login.php?status=incorrect");
+    exit;
     }
 }
 
