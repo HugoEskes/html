77030827e53474195f102a0308e0a82648295cca
HugoEskes <97437930+HugoEskes@users.noreply.github.com>
2023-02-02 21:36:02 +0100

    Debug

--- loggedin-reservation.php
+++ loggedin-reservation.php
@@ -33,11 +33,13 @@ if (isset($_POST['submit'])) {
     if ($connection->query($sql) === TRUE) {
           // send an email to the user that the account has been created
         $sql = "SELECT email FROM gebruikers WHERE gebruikerID = '$user_ID'"; 
-        $email = $connection->query($sql);
+        $email_result = $connection->query($sql);
+        $email_row = $result_result->fetch_assoc();
+        $email = $email_row["gebruikerID"];
 
         $to = $email;
         $subject = "Your SKI.I.P. reservation is confirmed!";
-        $message = "Hi $firstname $lastname, <br><br> Your SKI.I.P. reservation is confirmed!<br><br> <strong>Reservation details:</strong><br>Date: $date <br> Timeslot: $timeslot <br> Amount of spaces: $peolple <br> If you want to cancel or get an overview of all your reservations click ou want to change your password you can click <a href=\"https://webtech-ki59.webtech-uva.nl/loggedin-myreservations.php\">here</a> <br>The SKI.I.P. team";
+        $message = "Hi $firstname $lastname, <br><br> Your SKI.I.P. reservation is confirmed!<br><br> <strong>Reservation details:</strong><br>Date: $date <br> Timeslot: $timeslot <br> Amount of spaces: $people <br> If you want to cancel or get an overview of all your reservations click ou want to change your password you can click <a href=\"https://webtech-ki59.webtech-uva.nl/loggedin-myreservations.php\">here</a> <br>The SKI.I.P. team";
         $message = wordwrap($message, 70, "\r\n");
         $headers = "MIME-Version: 1.0" . "\r\n"; 
         $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n"; 
