1bd59149e64e455525bcb69d920b1d9ff812d266
HugoEskes <97437930+HugoEskes@users.noreply.github.com>
2023-02-02 17:54:27 +0100

    debug

--- admin_pages/admin_reservations.php
+++ admin_pages/admin_reservations.php
@@ -94,7 +94,7 @@ if (!$connection) {
 }
 
 // Select query
-$select_query = "SELECT * FROM reserveringen JOIN gebruikers ON reserveringen.gebruikerID = gebruikers.gebruikerID WHERE reserveringen.datum > GETDATE()";
+$select_query = "SELECT * FROM reserveringen JOIN gebruikers ON reserveringen.gebruikerID = gebruikers.gebruikerID WHERE reserveringen.datum > NOW()";
 $result_users = mysqli_query($connection, $select_query);
 
 echo "<table>";
