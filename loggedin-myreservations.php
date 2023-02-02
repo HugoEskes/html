b6d7345469d5150bd04bb9a18a1428ede712df86
Arthurvc-uva <Arthur.van.campenhout@student.uva.nl>
2023-02-02 17:04:28 +0100

    myreservations

--- loggedin-myreservations.php
+++ loggedin-myreservations.php
@@ -74,7 +74,7 @@ if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
 
 <?php
 
-require_once '../php/connection.php';
+require_once 'php/connection.php';
 
 // Check connection
 if (!$connection) {
