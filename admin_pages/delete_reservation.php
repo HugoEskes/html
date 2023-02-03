4de2fee4bd5ecc1acf943e3864d5b397c7651e9f
HugoEskes <97437930+HugoEskes@users.noreply.github.com>
2023-02-03 10:53:23 +0100

    Admin reservering verwijderen

--- admin_pages/delete_reservation.php
+++ admin_pages/delete_reservation.php
@@ -9,6 +9,23 @@
     // Get the form data
     $id = $_POST['id'];
 
+    $sql_find_information = "SELECT datum, tijdslot, personen FROM reserveringen WHERE reservatieID='$id'";
+    $reservation_result = mysqli_query($connection, $sql_find_information);
+
+    $reservation_row = $reservation_result->fetch_assoc();
+    $date = $reservation_row["datum"];
+    $timeslot = $reservation_row["tijdslot"];
+    $personen = $reservation_row["personen"];
+
+    $sql_tijden = "SELECT beschikbare_plekken FROM tijden WHERE datum='$date' and tijd='$timeslot'"; 
+    $availability_result = $connection->query($sql_tijden);
+    $availability_row = $availability_result->fetch_assoc();
+    $previous_availability = $availability_row["beschikbare_plekken"];
+    $new_availability = $previous_availability + $personen;
+
+    $sql_availability_update = "UPDATE tijden SET beschikbare_plekken='$new_availability' WHERE datum='$date' and tijd='$timeslot'";
+    mysqli_query($connection, $sql_availability_update);
+    
     // Delete query
     $delete_query = "DELETE FROM reserveringen WHERE reservatieID='$id'";
     $result = mysqli_query($connection, $delete_query);
