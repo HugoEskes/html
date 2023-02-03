903d3fd780e3bff5dc531b28a46f3e2fb079b749
HugoEskes <97437930+HugoEskes@users.noreply.github.com>
2023-02-03 17:09:05 +0100

    Kom op

--- table_maker.php
+++ table_maker.php
@@ -97,7 +97,7 @@ if (isset($_GET['availability_date'])) {
         color: gainsboro;
         table-layout: fixed;
         float: left;
-        width: 33px;
+        width: 33%;
         margin-right: 1%;
       }
       th, td {
