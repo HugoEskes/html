faf5c4bdb32a106b67d710f8e4eaf9bc22a6353f
HugoEskes <97437930+HugoEskes@users.noreply.github.com>
2023-02-03 16:53:20 +0100

    Met procenten dan maar

--- table_maker.php
+++ table_maker.php
@@ -103,7 +103,7 @@ if (isset($_GET['availability_date'])) {
       th, td {
         padding: 5px;
         text-align: center;
-        width: 84px;
+        width: 100%;
         overflow: hidden;
       }
 
